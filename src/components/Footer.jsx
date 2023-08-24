01f6d39345e719f52b026bb072537297754fe9a6
trungnd0111 <trung.nd19010036@st.phenikaa-uni.edu.vn>
2023-08-24 09:49:57 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Grid from './Grid'
+import logo from '../assets/images/Logo-2.png'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
